cf1b323af0f30b8da5dadb630b795cc6f697c009
Owen Stranathan <owen+github@appfigures.com>
2020-02-08 09:38:27 -0800

    Blah blah blah

--- Assets/Scripts/ExampleBehavior.cs
+++ Assets/Scripts/ExampleBehavior.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TestBehavior : InputListener
+public class ExampleBehavior : InputListener
 {
     // Start is called before the first frame update
     void Start()
